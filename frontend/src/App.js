e34213866dddfe9c061df57a7a29f8540c1af9a7
lorsim <lorsim96@gmail.com>
2021-02-16 12:30:34 +0530

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
-const App=() =>{
+import './App.css';
+import Header from './components/Header';
+
+function App() {
   return (
     <>
-     <h1>Welcome to social</h1>
+      <Header/>
+      <main>
+        <h1>Welcome to social</h1>
+      </main>
     </>
   );
 }
